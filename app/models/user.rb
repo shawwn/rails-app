ea5b25f5feaaa866711e0b2846feec2cef591f6e
Shawn Presser <shawnpresser@gmail.com>
2017-04-17 14:12:39 -0500

    User microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,12 @@
 class User < ApplicationRecord
-  # VALID_NAME_REGEX = /\A[a-zA-Z-_]+\z/
-  # validates :name, presence: true, length: { minimum: 2, maximum: 15 }, uniqueness: { case_sensitive: true }, format: { with: VALID_NAME_REGEX }
-  # validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
-  # has_secure_password
-  # validates :password, presence: true, length: { minimum: 4 }
+  has_many :microposts, dependent: :destroy
 
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
 
   # Returns the hash digest of the given string.
   def User.digest(string)
--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -5,8 +5,7 @@ class MicropostTest < ActiveSupport::TestCase
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
     @user.save
-    # This code is not idiomatically correct.
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do

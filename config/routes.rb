92ba0c92b1bc6787b4ea9e181c4655cfa1e38dbb
Shawn Presser <shawnpresser@gmail.com>
2017-04-17 11:32:25 -0500

    Add root route test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'home', to: 'static_pages#home'
   get 'help', to: 'static_pages#help'
   get 'about', to: 'static_pages#about'
-  root 'static_pages#home'
   get 'yo', to: 'application#yo'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

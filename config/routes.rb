aec2283d3f0fab8a81c01f831c424f7a7183cebc
Shawn Presser <shawnpresser@gmail.com>
2017-04-17 11:23:27 -0500

    Add /about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home', to: 'static_pages#home'
   get 'help', to: 'static_pages#help'
+  get 'about', to: 'static_pages#about'
   root 'static_pages#home'
   get 'yo', to: 'application#yo'
 

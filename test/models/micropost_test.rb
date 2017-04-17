3bbdd5596e04fd71b7fca50ade50dfd8cb63c87d
Shawn Presser <shawnpresser@gmail.com>
2017-04-17 14:08:34 -0500

    Micropost

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+    @user.save
+    # This code is not idiomatically correct.
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
 end

b7a2ece130059882ca3c706dbf20fc17d468a0d2
Shawn Presser <shawnpresser@gmail.com>
2017-04-17 13:16:01 -0500

    rails generate migration add_index_to_users_email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  VALID_NAME_REGEX = /\A[a-zA-Z-_]+\z/
+  validates :name, presence: true, length: { minimum: 2, maximum: 15 }, uniqueness: { case_sensitive: true }, format: { with: VALID_NAME_REGEX }
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 4 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170417181500) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
+  end
 
 end

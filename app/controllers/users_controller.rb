850ab37f4dd42cb555f50253ceb68a761d67570a
Shawn Presser <shawnpresser@gmail.com>
2017-04-17 14:26:48 -0500

    Micropost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
+  # before_action :logged_in_user, only: [:index, :edit, :update]
+
+  def index
+    @users = User.all
+    # @users = User.paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
+    # @microposts = @user.microposts.paginate(page: params[:page])
+    @microposts = @user.microposts
   end
 
   def new

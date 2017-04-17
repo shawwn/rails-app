ecc1f2801a2b39d7d9259750cc8b1e0c6a39bb3e
Shawn Presser <shawnpresser@gmail.com>
2017-04-17 13:26:07 -0500

    User test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
-  VALID_NAME_REGEX = /\A[a-zA-Z-_]+\z/
-  validates :name, presence: true, length: { minimum: 2, maximum: 15 }, uniqueness: { case_sensitive: true }, format: { with: VALID_NAME_REGEX }
+  # VALID_NAME_REGEX = /\A[a-zA-Z-_]+\z/
+  # validates :name, presence: true, length: { minimum: 2, maximum: 15 }, uniqueness: { case_sensitive: true }, format: { with: VALID_NAME_REGEX }
+  # validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
+  # has_secure_password
+  # validates :password, presence: true, length: { minimum: 4 }
+
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 4 }
+  validates :password, presence: true, length: { minimum: 6 }
 end
